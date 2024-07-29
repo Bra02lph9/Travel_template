e7c448f3535d9db35dd390c94d22af5406ab445f
El Mehdi Bennamrouche <bennamrouchex@gmail.com>
2024-07-29 11:29:10 +0100

    [alphaben] add readme.md content  & reaname Mainn Component to Main add some Screenshots

--- travel-site/src/Components/Main/Main.jsx
+++ travel-site/src/Components/Main/Main.jsx
@@ -1,6 +1,6 @@
 
       import React,{useEffect} from "react";
-      import './mainn.css';
+      import './Main.css';
       import img1 from '../../assets/img1.jpg';
       import img2 from '../../assets/img2.jpg';
       import img3 from '../../assets/img3.jpg';
@@ -128,7 +128,7 @@
         },
       ];
       
-      export default function Mainn() {
+      export default function Main() {
         useEffect(()=>{
           Aos.init({duration:2000})
        },[])
